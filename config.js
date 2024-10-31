c9cd16ebadeb7389a3513bcdb08d6d7c4b8cce29
lboss39 <ismatach5@gmail.com>
2024-10-31 05:33:32 +0100

    Update config.js

--- config.js
+++ config.js
@@ -8,15 +8,15 @@ import axios from 'axios';
 import moment from 'moment-timezone';
 
 global.owner = [
-  ['390371717717', 'nobody|'],
-  ['393518398856', 'nobody|'],
-  ['393755775755', 'nobody|'],
-  ['393755575577', 'nobody|'],
-  ['33760536110', 'nobody|'],
+  ['39 350 809 2093', 'nobody|'],
+  ['39 350 809 2093', 'nobody|'],
+  ['39 350 809 2093', 'nobody|'],
+  ['39 350 809 2093', 'nobody|'],
+  ['39 350 809 2093', 'nobody|'],
 ]
-global.suittag = ['390371717717'] 
-global.prems = ['390371717717'] 
-global.reportes_solicitudes = ['390371717717']
+global.suittag = ['39 350 809 2093'] 
+global.prems = ['39 350 809 2093'] 
+global.reportes_solicitudes = ['39 350 809 2093']
 
 global.packname = '(☞ﾟ∀ﾟ)☞'
 global.author = '★ცơɬῳɧąıɬą★'
